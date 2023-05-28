739ae6c99a81957f54a705f7daea4b7e2b6ce9d9
Marco Rampin <rampin.marco.17@gmail.com>
2023-05-28 16:07:28 +0200

    Added introduction section

--- javascript/script.js
+++ javascript/script.js
@@ -13,3 +13,17 @@ $(document).ready(function() {
     window.addEventListener("resize", resizeHeight);
   	window.addEventListener('load', loadFunctions());
 });
+
+/*navbar hiding
+var lastScrollTop;
+navbar = document.getElementById('navbar');
+window.addEventListener('scroll',function(){
+var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+if(scrollTop > lastScrollTop){
+navbar.style.top='-80px';
+}
+else{
+navbar.style.top='0';
+}
+lastScrollTop = scrollTop;
+}); */
